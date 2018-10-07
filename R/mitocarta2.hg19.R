3dfe853f12301f93e6d67eded692376cc8fdb2f1
ttriche <tim.triche@gmail.com>
2018-10-07 15:13:26 -0400

    BioC suggestions implemented

--- R/mitocarta2.hg19.R
+++ R/mitocarta2.hg19.R
@@ -7,13 +7,15 @@
 #' Calvo, S.E., Klauser, C.R., Mootha, V.K. MitoCarta2.0: an updated inventory of mammalian mitochondrial proteins (2015).  Nucleic Acids Research.
 #' 
 #' The publication is available from 
-#' http://nar.oxfordjournals.org/content/early/2015/10/07/nar.gkv1003.full
+#' \url{http://nar.oxfordjournals.org/content/early/2015/10/07/nar.gkv1003.full}
 #' 
 #' Please cite the publication if you use the MitoCarta database. 
 #' 
 #' @format A GRanges object.
 #' @source \url{http://www.broadinstitute.org/ftp/distribution/metabolic/papers/Pagliarini/MitoCarta2.0/Human.MitoCarta2.0.bed}
 #' 
+#' @import GenomicRanges
+#'
 #' @examples
 #' data(mitocarta2.hg19)
 #' show(mitocarta2.hg19)
