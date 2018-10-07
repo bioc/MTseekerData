49b834a9a98028e1849be36f587af448d0a578ea
ttriche <tim.triche@gmail.com>
2018-10-07 16:30:20 -0400

    explicitly import methods

--- R/MTseekerData.R
+++ R/MTseekerData.R
@@ -1,5 +1,7 @@
 #' Data for the MTseeker package examples. 
-#' 
+#'
+#' @rdname MTseekerData
+#'
 #' @export 
 .onAttach <- function (lib, pkgname="MTseekerData") {
   ## Put stuff here you want to run when your package is loaded
